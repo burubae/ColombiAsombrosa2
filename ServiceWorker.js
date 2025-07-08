df38b0279cb8e2d03b66421b08f42ef4449985e5
burubae <juanmiguelrestrepogonzalez@gmail.com>
2025-07-08 11:56:22 -0500

    final5.0

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,43 +1,6 @@
-const cacheName = "DigitalClay-ColombiaAsombrosa-v1.1";
-const contentToCache = [
-  "Build/ColombiAsombrosa.loader.js",
-  "Build/ColombiAsombrosa.framework.js.unityweb",
-  "Build/ColombiAsombrosa.data.unityweb",
-  "Build/ColombiAsombrosa.wasm.unityweb",
-  "TemplateData/style.css"
-];
 
-// 📦 Carga inicial al instalar
-self.addEventListener("install", event => {
-  console.log("[SW] Instalando...");
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log("[SW] Cacheando assets iniciales...");
-      return cache.addAll(contentToCache);
-    })
-  );
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
 });
 
-// ⚙️ Responde a solicitudes de red
-self.addEventListener("fetch", event => {
-  const url = new URL(event.request.url);
-
-  // 🚫 Ignora requests que no son GET (POST, etc.)
-  if (event.request.method !== "GET") return;
-
-  // 🚫 Ignora extensiones y protocolos raros
-  if (!url.protocol.startsWith("http")) return;
-
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) return response;
-
-      return fetch(event.request).then(res => {
-        return caches.open(cacheName).then(cache => {
-          cache.put(event.request, res.clone());
-          return res;
-        });
-      });
-    })
-  );
-});
